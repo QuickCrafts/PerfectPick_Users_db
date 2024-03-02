95aa58882ca925ecd6e99ef780baa22ef716f47e
sescandon <sescandon@unal.edu.co>
2024-03-02 11:16:53 -0500

    Agregar archivos de proyecto.

---
+++ PerfectPickUsers_DB/dbo/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [User_Name]     NVARCHAR (50) NOT NULL,
+    [User_Surname]  NVARCHAR (50) NOT NULL,
+    [User_Email]    VARCHAR (320) NOT NULL,
+    [User_Password] VARCHAR (60)  NOT NULL,
+    [User_isAdmin]  BIT           NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([User_Email] ASC)
+);
+
