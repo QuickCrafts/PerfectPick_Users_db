15f822bea7d4bcafea685c3a610e64a1e6e4bd7b
sescandon <sescandon@unal.edu.co>
2024-03-07 16:15:09 -0500

    Adapted to new model view

--- PerfectPickUsers_DB/dbo/Tables/Users.sql
+++ PerfectPickUsers_DB/dbo/Tables/Users.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Users] (
-    [id_user]      INT          NOT NULL,
+    [id_user]      INT          NOT NULL IDENTITY(1,1),
     [email]        VARCHAR (50) NOT NULL,
     [password]     VARCHAR (60) NOT NULL,
     [first_name]   VARCHAR (50) NOT NULL,
@@ -7,9 +7,11 @@
     [birthdate]    VARCHAR (30) NOT NULL,
     [gender]       VARCHAR (1)  NOT NULL,
     [created_time] VARCHAR (30) NOT NULL,
+    [avatar_url]   VARCHAR (100) NULL,
     [id_country]   INT          NOT NULL,
+    [verified]     BIT          NOT NULL,
+    [setup]        BIT          NOT NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([id_user] ASC),
-    CHECK ([gender]='O' OR [gender]='F' OR [gender]='M'),
     CHECK ([gender]='O' OR [gender]='F' OR [gender]='M')
 );
 
