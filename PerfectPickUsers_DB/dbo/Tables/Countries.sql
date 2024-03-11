9b6f63f6b6e0db808eedc3fd2272962ac3ab5805
sescandon <sescandon@unal.edu.co>
2024-03-11 09:47:28 -0500

    Adapted to model changes (Again xd)

--- PerfectPickUsers_DB/dbo/Tables/Countries.sql
+++ PerfectPickUsers_DB/dbo/Tables/Countries.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Countries] (
-    [id_country] INT          NOT NULL,
+    [id_country] INT IDENTITY(1,1) NOT NULL,
     [name]       VARCHAR (70) NULL,
     [code_2]     VARCHAR (2)  NULL,
     [code_3]     VARCHAR (3)  NULL,
--- PerfectPickUsers_DB/dbo/Tables/Users.sql
+++ PerfectPickUsers_DB/dbo/Tables/Users.sql
@@ -1,17 +1,19 @@
 ﻿CREATE TABLE [dbo].[Users] (
-    [id_user]      INT          NOT NULL IDENTITY(1,1),
-    [email]        VARCHAR (50) NOT NULL,
-    [password]     VARCHAR (60) NOT NULL,
-    [first_name]   VARCHAR (50) NOT NULL,
-    [last_name]    VARCHAR (50) NOT NULL,
-    [birthdate]    VARCHAR (30) NOT NULL,
-    [gender]       VARCHAR (1)  NULL,
-    [created_time] VARCHAR (30) NOT NULL,
+    [id_user]      INT           IDENTITY (1, 1) NOT NULL,
+    [email]        VARCHAR (50)  NOT NULL,
+    [password]     VARCHAR (60)  NOT NULL,
+    [first_name]   VARCHAR (50)  NOT NULL,
+    [last_name]    VARCHAR (50)  NOT NULL,
+    [birthdate]    VARCHAR (30)  NOT NULL,
+    [gender]       VARCHAR (1)   NULL,
+    [created_time] VARCHAR (30)  NOT NULL,
     [avatar_url]   VARCHAR (100) NULL,
-    [id_country]   INT          NULL,
-    [verified]     BIT          NOT NULL,
-    [setup]        BIT          NOT NULL,
+    [id_country]   INT           NULL,
+    [verified]     BIT           NOT NULL,
+    [setup]        BIT           NOT NULL,
+    [role]		   BIT           NOT NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([id_user] ASC),
-    CHECK ([gender]='O' OR [gender]='F' OR [gender]='M')
+    CHECK ([gender]='O' OR [gender]='F' OR [gender]='M'),
+    CONSTRAINT [UniqueEmail_Users] UNIQUE NONCLUSTERED ([email] ASC)
 );
 
